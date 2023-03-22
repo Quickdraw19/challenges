d96c16e78121512ab5c80f4eb27d6f906b39681a
Larry <larry@qdraw.net>
2023-03-22 03:43:51 -0600

    Update tower-breakers.js

--- tower-breakers.js
+++ tower-breakers.js
@@ -1,1 +1,23 @@
+/*
+ * Complete the 'towerBreakers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ */
 
+function towerBreakers(n, m) {
+    let winner = 0;
+    
+    // If towers are only 1 high: Player 2 always wins
+    // If odd number of towers, Player 1 always wins
+    // If even number of towers, Player 2 always wins
+    if (m === 1) {
+        return 2;
+    }
+    
+    winner = (n % 2 === 0) ? 2 : 1;
+    
+    return winner;
+}
