40282efa5ec77c32ceab45b439d39c4728585f08
Larry <larry@qdraw.net>
2023-03-22 03:57:05 -0600

    Update subarray-division-1.js

--- subarray-division-1.js
+++ subarray-division-1.js
@@ -1,1 +1,25 @@
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
 
+function birthday(s, d, m) {
+    let barLen = s.length;
+    let count = 0;
+    
+    for (let i = 0; i <= barLen-m; i++) {
+        let seg = s.slice(i, i + m);
+        
+        let sum = seg.reduce((t, n) => t + n, 0);
+        if (sum == d) {
+            count++;
+        }
+    }
+    
+    return count;
+}
