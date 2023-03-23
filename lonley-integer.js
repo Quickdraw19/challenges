8268a981e6780b0da4b9b472f294a3b2c3ca6fbb
Larry <larry@qdraw.net>
2023-03-23 00:33:44 -0600

    Update lonley-integer.js

--- lonley-integer.js
+++ lonley-integer.js
@@ -1,14 +1,26 @@
-function lonelyinteger(a) {
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+function lonelyinteger(arr) {
     let loner, seg;
-    a.sort()
-    for (let i = 0; i <= a.length; i = i + 2) {
-        seg = a.slice(i, i + 2);
+    arr.sort((a,b) => a - b); // Sort numerically
+    
+    for (let i = 0; i <= arr.length; i = i + 2) {
+        // Get a segment of the array two digits at a time
+        seg = arr.slice(i, i + 2);
+
+        // If both numbers in the segment are equal, move to the next set
         if (seg[0] == seg[1]) {
             continue;
         } else {
+            // If both numbers in the segment don't match, then the first number must be breaking the pattern.
             loner = seg[0];
             break;
         }
     }
+    
     return loner;
 }
