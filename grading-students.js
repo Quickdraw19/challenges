7e136eb6b1f594ad9a4da0f62aab6f8a6dfb2fa6
Larry <larry@qdraw.net>
2023-03-24 00:11:20 -0600

    Update grading-students.js

--- grading-students.js
+++ grading-students.js
@@ -4,14 +4,13 @@
  * The function is expected to return an INTEGER_ARRAY.
  * The function accepts INTEGER_ARRAY grades as parameter.
  */
-
 function gradingStudents(grades) {
     let rGrades = [];
 
     for (let i = 0; i < grades.length; i++) {
-        let stepUp = Math.ceil(grades[i]/10) * 10;
+        let stepUp = Math.ceil(grades[i]/5) * 5;
         
-        if (grades[i] > 40 && stepUp - grades[i] < 3) {
+        if (grades[i] > 37 && stepUp - grades[i] < 3) {
             rGrades[i] = stepUp;
         } else {
             rGrades[i] = grades[i];
