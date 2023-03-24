c315a265d509e87488879fa26ff87f2e4786c290
Larry <larry@qdraw.net>
2023-03-24 00:38:31 -0600

    Update flipping-bits.js

--- flipping-bits.js
+++ flipping-bits.js
@@ -16,18 +16,12 @@ function flippingBits(number) {
     
     // Iterate through each of the 32 digits
     for (let i = 0; i < 32; i++) {
-        // If the digit is 1, then change it to 0.
-        if (binNum[i]) {
-            //binNumFlipped[i] = binNum[i] ^ 1;
-            binNumFlipped[i] = 0;
-        } else { // Change it to 1.
-            binNumFlippped[i] = 1;
-        }
+        binNumFlipped[i] = binNum[i] ^ 1;
     }
     
     // Convert the array of bits into a string
-    binNumFlippped = binNumFlippped.join("");
+    binNumFlipped = binNumFlipped.join("");
     
     // Convert the base2 version of the number back into base10 respresentation.
-    return parseInt(nFlipBin, 2);
+    return parseInt(binNumFlipped, 2);
 }
