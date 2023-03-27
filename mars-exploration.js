b034bdb61f7856cc069ce73eb275e13ccaba2da6
Larry <larry@qdraw.net>
2023-03-26 21:24:49 -0600

    Update mars-exploration.js

--- mars-exploration.js
+++ mars-exploration.js
@@ -1,1 +1,26 @@
+/*
+ * Complete the 'marsExploration' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
 
+function marsExploration(msg) {
+    let errors = 0;
+    let msgSegments = msg.match(/.{1,3}/g);
+    
+    // I didn't optimise this
+    msgSegments.forEach((seg) => {
+        if (seg[0] !== "S") {
+            errors++
+        }
+        if (seg[1] !== "O") {
+            errors++
+        }
+        if (seg[2] !== "S") {
+            errors++
+        }
+    });
+    
+    return errors;
+}
