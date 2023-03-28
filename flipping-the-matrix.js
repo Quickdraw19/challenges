7ae5e661622a64c23df736da7484a824ea20e7e0
Larry <larry@qdraw.net>
2023-03-27 22:24:50 -0600

    Update flipping-the-matrix.js

--- flipping-the-matrix.js
+++ flipping-the-matrix.js
@@ -5,33 +5,25 @@
 * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
 */
 function flippingMatrix(matrix) {
-  let mlen = matrix.length;
-  
-  // Size of the matrix
-  let qlen = mlen / 2;
-  
-  // Size of the quadrant
+  let matrixSize = matrix.length;
+  let quadrantSize = matrixSize / 2;
   let total = 0;
   
-  for (let row = 0; row < qlen; row++) {
-    for(let col = 0; col < qlen; col++) {
-      let hiVal = 0;
+  for (let row = 0; row < quadrantSize; row++) {
+    for(let col = 0; col < quadrantSize; col++) {
+      let highestTotal = 0;
+      let oppCol = (matrixSize - col) - 1;
+      let oppRow = (matrixSize - row) - 1;
       
-      // Reset the highest value on each iteration
-      // Get the value of the current location
-      hiVal = matrix[row][col];
+      highestTotal = Math.max(
+        matrix[row][col],
+        matrix[row][oppCol],
+        matrix[oppRow][col],
+        matrix[oppRow][oppRow],
+        highestTotal;
+       );
       
-      // See if the opposite number in the column is a higher value
-      hiVal = Math.max(matrix[row][mlen - col - 1], hiVal);
-      
-      // See if the oppostite number in the row is a higher value
-      hiVal = Math.max(matrix[mlen - row - 1][col], hiVal);
-      
-      // See if the opposite diagonal number is a higher value
-      hiVal = Math.max(matrix[mlen - row - 1][mlen - col - 1], hiVal);
-      
-      // Add the highest value to the total
-      total += hiVal;
+      total += highestTotal;
     }
   }
   
