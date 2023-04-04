8ee78212a07eca61f9d7ae1cdf5be611558b0e54
Larry <larry@qdraw.net>
2023-04-03 21:15:34 -0600

    Update maximum-perimeter-triangle.js

--- maximum-perimeter-triangle.js
+++ maximum-perimeter-triangle.js
@@ -1,1 +1,11 @@
+/*
+ * Complete the 'maximumPerimeterTriangle' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY sticks as parameter.
+ */
 
+function maximumPerimeterTriangle(sticks) {
+    // For three lines to make a triangle, the sum of the two shorter lengths must be greater than the other 3rd length.
+    return -1;
+}
