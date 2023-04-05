3d6797d6ea424138ff99d91316541f01f43c03a6
Larry <larry@qdraw.net>
2023-04-05 17:33:18 -0600

    Update drawing-book.js

--- drawing-book.js
+++ drawing-book.js
@@ -1,1 +1,24 @@
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
 
+function pageCount(length, page) {
+    let currPage = 1;
+    
+    if (length / 2 < page) {
+        page = length - page + 1;
+    }
+    
+    if (currPage === 1) {
+        while (currPage < page) {
+            currPage += 2;
+        }
+    }
+    
+    return Math.floor(currPage / 2);
+}
