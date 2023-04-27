b11544e9c41028b44c08cf371ee00275f8d9dbe0
Larry <larry@qdraw.net>
2023-04-26 22:44:53 -0600

    Update left-rotation.js

--- left-rotation.js
+++ left-rotation.js
@@ -1,1 +1,9 @@
+function challenge(d, arr) {
+    let leftNums = arr.slice(0, d);
+    let rightNums = arr.slice(d);
+    let shiftedArr = rightNums.concat(leftNums);
 
+    return shiftedArr;
+}
+
+let result = challenge(4, [1, 2, 3, 4, 5]);
