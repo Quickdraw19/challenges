0b276b89b5af175dbc47737013366405df8f7d22
Larry <larry@qdraw.net>
2023-04-29 19:43:42 -0600

    Update separate-the-numbers.js

--- separate-the-numbers.js
+++ separate-the-numbers.js
@@ -1,1 +1,23 @@
+/*
+ * Complete the 'separateNumbers' function below.
+ *
+ * The function accepts STRING s as parameter.
+ */
 
+function separateNumbers(s) {
+    if (s.length === 1) {
+        console.log("NO");
+    }
+    
+    let subjectNum = s[0];
+    
+    for (let i = 1; i < s.length; i++) {
+        if (subjectNum + 1 == s[i]) {
+            subjectNum = s[i];
+        } else {
+            return "NO";
+        }
+    }
+    
+    console.log("YES " + s[0]);
+}
