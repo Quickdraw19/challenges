1d0dfdd6956cd579192e32f5c72c28a205fd0e43
Larry <larry@qdraw.net>
2023-07-02 19:09:24 -0600

    Update minimum-absolute-difference-array.js

--- minimum-absolute-difference-array.js
+++ minimum-absolute-difference-array.js
@@ -1,1 +1,25 @@
+// Minimum Absolute Difference in an Array
+// minimumAbsoluteDifference(arr)
+// TWO TEST CASES TAKING TOO LONG AND NOT GIVING THE ARRAY PASSED IN. NEED TO OPTIMIZE.
+function myfunction(arr) {
+    let minDiff = -1;
+    const arrLen = arr.length;
 
+    for (let i = 0; i < arrLen; i++) {
+        for (let j = 0; j < arrLen; j++) {
+            if (i === j) continue;
+
+            let comp = Math.abs(arr[i] - arr[j]);
+            if (comp < minDiff || minDiff === -1) {
+                minDiff = comp;
+            }
+        }
+    }
+
+    return minDiff;
+}
+
+let funcResult = myfunction([-2, 2, 4]);
+let expectedResult = 2;
+let test1 = (funcResult === expectedResult) ? "PASS" : "FAIL";
+console.log("Test 1: " + test1);
