164b63b8c9134d6ec6b94f1fd435b5a02ea68662
Larry <larry@qdraw.net>
2023-08-12 16:46:02 -0600

    Update and rename grid-challenge to grid-challenge.js

---
+++ grid-challenge.js
@@ -0,0 +1,35 @@
+/*
+Given a square grid of characters in the range ascii[a-z], rearrange 
+elements of each row alphabetically, ascending. Determine if the columns 
+are also in ascending alphabetical order, top to bottom. Return YES if 
+they are or NO if they are not.
+
+Example:
+The grid is illustrated below.
+a b c
+a d e
+e f g
+The rows are already in alphabetical order. The columns a a e, b d f and c e g 
+are also in alphabetical order, so the answer would be YES. Only elements 
+within the same row can be rearranged. They cannot be moved to a different row.
+
+gridChallenge() has the following parameter(s):
+  string grid[n]: an array of strings
+Returns
+  string: either YES or NO
+Input
+  Each of the next  sets of lines are described as follows:
+  - The first line contains , the number of rows and columns in the grid.
+  - The next  lines contains a string of length 
+
+Constraints
+  Each string consists of lowercase letters in the range ascii[a-z]
+
+Output Format
+  For each test case, on a separate line print YES if it is possible to rearrange 
+  the grid alphabetically ascending in both its rows and columns, or NO otherwise.
+*/
+
+function gridChallenge(grid) {
+
+}
