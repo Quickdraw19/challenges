0a110215c8c70e91228c8a8d91d69f55cb372e37
Larry <larry@qdraw.net>
2023-12-22 16:25:49 -0700

    Update grid-challenge.js

--- grid-challenge.js
+++ grid-challenge.js
@@ -1,4 +1,14 @@
 /*
+Programming concepts to brush up on:
+How does OAuth work and what are the security vulnerabilites?
+What is a static VARIABLE in PHP?
+Best practices for using const vs let vs var: I bookmarked an article on my phone
+TailwindCSS
+----
+How does cross-site scripting work? Look into this- https://github.com/beefproject/beef
+How does cross-site request forgery (CCRF) work?
+*******
+=====================================================================
 ONE WEEK, DAY 4 TEST
 Given a square grid of characters in the range ascii[a-z], rearrange 
 elements of each row alphabetically, ascending. Determine if the 
@@ -28,7 +38,7 @@ Input Format
 The first line contains , the number of testcases.
 Each of the next  sets of lines are described as follows:
 - The first line contains , the number of rows and columns in the grid.
-- The next  lines contains a string of length 
+- The next lines contains a string of length 
 
 Constraints
 Each string consists of lowercase letters in the range ascii[a-z]
@@ -45,7 +55,8 @@ STDIN   Function
 -----   --------
 1       t = 1
 5       n = 5
-ebacd   grid = ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']
+grid = ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']
+ebacd
 fghij
 olmkn
 trpqs
@@ -65,24 +76,60 @@ This fulfills the condition since the rows 1, 2, ..., 5
 and the columns 1, 2, ..., 5 are all alphabetically sorted.
 */
 
-const challenge = (s) => {
+const challenge = (data) => {
+  let grid = data
+  let grid2 = []
 
+  grid.forEach(element => {
+    let set = element.split("")
+    set.sort()
+    grid2.push(set)
+  });
+
+  let iSize = grid2[0].length
+  let jSize = grid2.length
+  
+  for (let i = 0; i < iSize; i++) {
+    let tempArr = []
+    for (let j = 0; j < jSize; j++) {
+      tempArr.push(grid2[j][i])
+    }
+
+    let tempOrig = [...tempArr]
+    let tempSorted = [...tempArr.sort()]
+    if (JSON.stringify(tempOrig) === JSON.stringify(tempSorted)) {
+      continue
+    } else {
+      //console.log("NO")
+      return "NO"
+    }
+  }
+
+  //console.log("YES")
+  return "YES"
 }
 
 // TEST DATA [{arguments}: {expected result}]
 const testCases = {
-
+  
 }
 
 // TEST CODE
-let counter = 0
-for (const testData in testCases) {
-  if (testCases.hasOwnProperty(testData)) {
-    const expected = testCases[testData]
-    // console.log(`Test '${testData}': `, (challenge(testData) === expected) ? "PASS" : "FAIL")
-    const result = challenge(testData)
-    console.log(`Test '${counter++}': `, (result === expected) ? "PASS" : "FAIL")
-    // console.log(`Test '${counter++}': `, (result === expected) ? "PASS" : "FAIL", ` -- expected ${expected}, returned ${result}`)
-    //console.log(expected)
-  }
-}
+// let counter = 0
+// for (const testData in testCases) {
+//   if (testCases.hasOwnProperty(testData)) {
+//     const expected = testCases[testData]
+//     // console.log(`Test '${testData}': `, (challenge(testData) === expected) ? "PASS" : "FAIL")
+//     const result = challenge(testData)
+//     console.log(`Test '${counter++}': `, (result === expected) ? "PASS" : "FAIL")
+//     // console.log(`Test '${counter++}': `, (result === expected) ? "PASS" : "FAIL", ` -- expected ${expected}, returned ${result}`)
+//     //console.log(expected)
+//   }
+//}
+
+challenge(['abc','ade','efg']) //YES
+challenge(['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']) //YES
+challenge(['eabcd', 'fghij', 'olkmn', 'trpqs', 'xywuv']) //YES
+challenge(['abc', 'lmp', 'qrt']) //YES
+challenge(['mpxz', 'abcd', 'wlmf']) //NO
+challenge(['abc', 'hjk', 'mpq', 'rtv']) //YES
